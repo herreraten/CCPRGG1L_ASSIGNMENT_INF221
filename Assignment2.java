7ba8c6b08a517038682d47716185cc09e0c8463b
herreraten <112837135+herreraten@users.noreply.github.com>
2022-10-03 11:21:22 +0800

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,42 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+      for (int i = 5; i != 0; i--) {
+
+            if (i == 2) {
+                System.out.println("end of first name");
+                break;
+            }
+
+            System.out.println("CHRISTENNE");
+        }
+            
+        
+        
+        int nicknameCount = 5;
+
+        while (nicknameCount != 0) {
+            
+             nicknameCount--;
+             
+           if (nicknameCount == 2) {
+                System.out.println("Go ahead i will skip this third nickname. i will continue on the next nickname!");
+                continue;
+            }
+            System.out.println("TENTEN");
+            
+           
+        
+        
+             
+        }
+        
+         int nameCount = 5;
+
+        do {
+            System.out.println("HERRERA");
+            nameCount--;
+        } while (nameCount != 0);
+        
+    }
+}
